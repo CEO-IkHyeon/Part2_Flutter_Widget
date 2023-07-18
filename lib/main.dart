4da090be599b5437ce2a75a99fbc6018e2eae923
IkHyeon <andantej281@gmail.com>
2023-07-18 17:17:17 +0900

    Part2_ch02_01_Widget_SingleChildScrollView 학습

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ void main() {
 //         width: 300,
 //         height: 300,
 //         padding: EdgeInsets.fromLTRB(10, 12, 10, 12), // pading : container 내부에서 상하좌우에서 각 모서리와 간격 조절
-//         //margin: EdgeInsets.symmetric(vertical: 24, horizontal: 30), // margin : container 자체가 앱 모서리들과 간격 조절
+//         //margin: EdgeInsets.symmetric(horizontal: 24, horizontal: 30), // margin : container 자체가 앱 모서리들과 간격 조절
 //         decoration: BoxDecoration(
 //           // container에서 boxdecoration의 경우 밖에 또는 doxdeco 둘 중 하나에만 color가 선언되어야 함 -> boxdeco안에서도 color를 선언할 수 있기 때문
 //           color: Color(0xFF85D07B),   // FF : 투명도 , 이후 2자리씩 RGB값을 나타냄
@@ -57,37 +57,58 @@ class Body extends StatelessWidget {
     // -> 좌우의 폭을 지정할 수 없다 -> container로 wrap하고 width을 무제한으로 주고 crossAxisAligment로 좌, 중 ,우 조정 가능
     // Row는 Column과 유사하지만 수평으로 배열해준다 -> 좌우에 대해서만 지정한다. mainAxisAlignment로 start(좌), center(중), end(우)로 위치 조정 가능
     // -> 상, 중, 하로 위치 조절은 불가능 -> container로 wrap하고 height를 무제한으로 주고 crossAxisAligment로 상, 중, 하 조정 가능
-    return Container(
-      //width: double.infinity,
-      height: double.infinity,
-      // child: Column(
+
+    // SingleChildScrollView : 하나의 자식 객체를 갖는 scroll view -> 높이가 초과할 거 같다면 scroll로 구현해 erorr 해결
+    // child가 Column인 경우 문제 없지만 Row로 하면 따로 선언해줄게 있음
+    // ctrl + r -> 한번에 replace 가능
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
       child: Row(
-        // min : column의 상하 값은 딱 child container 크기에 맞춰짐 -> mainAxisAlignment: center가 의미가 없음
-        // max : 최대 높이를 Column이 차지함
-        mainAxisSize: MainAxisSize.max,
-        // 해당 container 기준에서 중앙에 Column이 위
-        mainAxisAlignment: MainAxisAlignment.center,  // 상하 기준 중앙에 위치
-        crossAxisAlignment: CrossAxisAlignment.center, // start : 좌측에 붙음 / end : 우측에 붙음 / center : 중앙
+        mainAxisAlignment: MainAxisAlignment.center,
+
         children: [
           Container(
             width: 100,
-            height: 80,
-            color: Colors.red,
-            child: Text('Container1'),
+            height: 100,
+            color: Colors.grey,
+            margin: EdgeInsets.symmetric(horizontal: 8),
           ),
           Container(
             width: 100,
-            height: 80,
-            color: Colors.green,
-            child: Text('Container2'),
-          ),
-          Container(
+            height: 100,
+            color: Colors.grey,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),Container(
             width: 100,
-            height: 80,
-            color: Colors.blue,
-            child: Text('Container3'),
+            height: 100,
+            color: Colors.grey,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),Container(
+            width: 100,
+            height: 100,
+            color: Colors.grey,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),Container(
+            width: 100,
+            height: 100,
+            color: Colors.grey,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),Container(
+            width: 100,
+            height: 100,
+            color: Colors.grey,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),Container(
+            width: 100,
+            height: 100,
+            color: Colors.grey,
+            margin: EdgeInsets.symmetric(horizontal: 8),
+          ),Container(
+            width: 100,
+            height: 100,
+            color: Colors.grey,
+            margin: EdgeInsets.symmetric(horizontal: 8),
           ),
-
         ],
       ),
     );
