668884b35bd77d272665983f26370d4ad7dba98e
Ik Hyeon Cho <andantej99@ajou.ac.kr>
2023-07-24 12:48:32 +0900

    02_05_02_Flutter의 CallBack 이벤트 1. VoidCallback, Function() 2. InkWell(), GestureDetector()

--- lib/main.dart
+++ lib/main.dart
@@ -2,223 +2,84 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  // 가장 기본이 되는 앱 : MaterialApp (widget tree에 최상위에 위치
-
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: const Text('다양한 Flutter의 입력 알아보기'),
+        title: const Text('Flutter의 CallBack'),
       ),
       body: const Body(),
     ),
   ));
 }
 
+// callback : 특정 동작 이후에 다른 widget에서 처리
+// Stateful widget일 때 발생한 state를 해당 widget 안에서만 처리 가능 -> widget을 벗어나면 해당 state에 접근하기 어려움
+// -> callback으로 어느정도 해결 가능
 class Body extends StatelessWidget {
   const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // const를 안하면 물결모양이 생김 --> 변경될 일 없으니 const로 선언해달라는 의미
-    return const Column(
-      children: [
-        TestCheckBox(),
-        TestRadioButton(),
-        TestSlider(),
-        TestSwitch(),
-        TestPopupMenu(),
-      ],
-    );
+    return TestWidget();
   }
 }
 
-////////////////////////////////////////////////////////////////////////////////
-// 1. CheckBox
-class TestCheckBox extends StatefulWidget {
-  const TestCheckBox({super.key});
+class TestWidget extends StatefulWidget {
+  const TestWidget({super.key});
 
   @override
-  State<TestCheckBox> createState() => _TestCheckBoxState();
+  State<TestWidget> createState() => _TestWidgetState();
 }
 
-class _TestCheckBoxState extends State<TestCheckBox> {
-  // checkBox의 값을 담을 list 선언
-  late List<bool> values;
-
-  // 주로 initState method를 통해 초기화하는 것이 좋음
-  @override
-  void initState() {
-    super.initState();
-    values = [false, false, false];
-  }
+class _TestWidgetState extends State<TestWidget> {
+  int value = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Checkbox(
-            value: values[0],
-            onChanged: (value) => changeValue(0, value: value)),
-        // value에 Null이 들어오면 false로 바꿔줌
-        Checkbox(
-            value: values[1],
-            onChanged: (value) => changeValue(1, value: value)),
-        Checkbox(
-            value: values[2],
-            onChanged: (value) => changeValue(2, value: value)),
-      ],
-    );
+    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Text(
+        'Count : $value',
+        style: const TextStyle(fontSize: 30),
+      ),
+      TestButton(addCounter), //addCounter() 함수의 이름을 넘겨주는 것이지 실행을 넘기는게 아님 -> ()는 빼고 입력
+    ]);
   }
 
-  //매개변수로 NULL이 들어오면 자동적으로 false로 바꿔준다
-  void changeValue(int index, {bool? value = false}) {
-    setState(() {
-      values[index] = value!; // ! : NUll이 아니라는 의미?
-    });
-  }
+  // void addCounter() => setState(() => ++value);
+
+  // 값을 같이 넘길 때는 VoidCallback이 아닌 Function(int)로
+  void addCounter(int addValue) => setState(() => value = value + addValue);
 }
 
-////////////////////////////////////////////////////////////////////////////////
-// 2. Radio : eNum이랑 같이 자주 쓰임
-// 줄 정리 : ctrl + alt + L
-class TestRadioButton extends StatefulWidget {
-  const TestRadioButton({super.key});
+// TestButton에 addCount() 를 넘겨줘야함 -> 보통 함수를 넘겨줄 땐 VoidCallBack 사용
+class TestButton extends StatelessWidget {
+  const TestButton(this.callback, {super.key});
 
-  @override
-  State<TestRadioButton> createState() => _TestRadioButtonState();
-}
-
-// 객체 이름 바꿀 때 shift + F6
-enum TestValue {
-  test1,
-  test2,
-  test3;
-}
-
-class _TestRadioButtonState extends State<TestRadioButton> {
-  // ? : 초기에 값이 없을 수도 있다
-  TestValue? selectValue;
-
+  // final VoidCallback callback;
+  final Function(int) callback;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // RadioButton 옆에 text 쓰기
-        ListTile(
-          leading: Radio<TestValue>(
-              value: TestValue.test1,
-              groupValue: selectValue,
-              onChanged: (value) => setState(() => selectValue = value!)),
-          title: Text(TestValue.test1.name),
-          // onTap : 꼭 radio button이 아닌 해당 라인을 클릭해도 인식하게 함 -> 클릭 범위를 확장
-          onTap: () => setState(() {
-            if (selectValue != TestValue.test1) {
-              selectValue = TestValue.test1;
-            }
-          }),
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      width: double.infinity,
+      child: InkWell(
+        onTap: () => callback.call(1),
+        onDoubleTap: () => callback.call(5),
+        onLongPress: () => callback.call(10),
+        child: Center(
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+            decoration: BoxDecoration(border: Border.all()),
+            child: const Text(
+              'Up Counter',
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
         ),
-        Radio<TestValue>(
-            value: TestValue.test2,
-            groupValue: selectValue,
-            onChanged: (value) => setState(() => selectValue = value!)),
-        Radio<TestValue>(
-            value: TestValue.test3,
-            groupValue: selectValue,
-            onChanged: (value) => setState(() => selectValue = value!)),
-      ],
+      ),
     );
   }
 }
 
-////////////////////////////////////////////////////////////////////////////////
-// 3. Slider
-class TestSlider extends StatefulWidget {
-  const TestSlider({super.key});
-
-  @override
-  State<TestSlider> createState() => _TestSliderState();
-}
-
-class _TestSliderState extends State<TestSlider> {
-  double value = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        //Slider 위에 Text
-        Text('${value.round()}'),
-        Slider(
-          value: value,
-          // int값을 double로 변환 : value as double
-          onChanged: (newValue) => setState(() => value = newValue),
-          divisions: 100,
-          max: 100,
-          min: 0,
-          label: '${value.round()}',
-          // slider 잡고 움직일 때 label로 값 표시
-          activeColor: Colors.grey, // slider 색
-        ),
-      ],
-    );
-  }
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// 4. Switch()
-class TestSwitch extends StatefulWidget {
-  const TestSwitch({super.key});
-
-  @override
-  State<TestSwitch> createState() => _TestSwitchState();
-}
-
-class _TestSwitchState extends State<TestSwitch> {
-  bool value1 = false;
-  bool value2 = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Switch(
-          value: value1,
-          onChanged: (newValue) => setState(() => value1 = newValue),
-        ),
-        CupertinoSwitch(
-          value: value2,
-          onChanged: (newValue) => setState(() => value2 = newValue),
-        ),
-      ],
-    );
-  }
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// 5. PopupMenuButton()
-class TestPopupMenu extends StatefulWidget {
-  const TestPopupMenu({super.key});
-
-  @override
-  State<TestPopupMenu> createState() => _TestPopupMenuState();
-}
-
-class _TestPopupMenuState extends State<TestPopupMenu> {
-  TestValue selectValue = TestValue.test1;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // PopupMenu에서 선택한 것을 보여주기 위해 Text()
-        Text(selectValue.name),
-        PopupMenuButton(itemBuilder: (context) {
-          return TestValue.values
-              .map((value) => PopupMenuItem(value: value, child: Text(value.name)))
-              .toList();
-        },
-        onSelected: (newValue) => setState(() => selectValue = newValue),),
-      ],
-    );
-  }
-}
+// 1. GestureDetector : 기본적인 입력 감지기 -> onTap 자주 쓰임 (클릭하는 것을 감지)
+// 2. Ink : GestureDetector 와 유사하지만 클릭 시 화영 번지는 듯한 느낌 줌 (UX)
