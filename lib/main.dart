6c2b1ec6fe28dfb10527847cf8347cae50627eab
IkHyeon <andantej281@gmail.com>
2023-07-25 11:20:34 +0900

    02_07_01_페이지에서 페이지 이동 1 1. Scaffold에서 bottomNavigationBar 이용

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,69 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+
 const assetImagePath = 'assets/images';
 const myFaceImage = '$assetImagePath/myFace.jpg';
+
 void main() {
   // 가장 기본이 되는 앱 : MaterialApp (widget tree에 최상위에 위치
 
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter에서 로컬 데이터 사용하기'),
-        centerTitle: true,
-      ),
-      body: const Body(),
-    ),
+  runApp(const MaterialApp(
+    home: HomeWidget(),
   ));
 }
 
-//
-class Body extends StatelessWidget {
-  const Body({super.key});
+class HomeWidget extends StatefulWidget {
+  const HomeWidget({super.key});
+
+  @override
+  State<HomeWidget> createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  late int index;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    index = 0;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Image.asset(myFaceImage),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter에서 화면 이동하기 1'),
+        centerTitle: true,
+      ),
+      body: homeBody(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'User'),
+        ],
+        currentIndex: index,
+        onTap: (newIndex) => setState(() => index = newIndex),
+      ),
     );
   }
-}
 
+  Widget homeBody() {
+    switch (index) {
+      case 1:
+        return const Center(
+          child: Icon(Icons.search, size: 100),
+        );
+      case 2:
+        return const Center(
+          child: Icon(Icons.person, size: 100),
+        );
+      case 0:
+      default:
+        return const Center(
+          child: Icon(Icons.home, size: 100),
+        );
+    }
+  }
+}
