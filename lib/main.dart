a8f43d4356d5888c41e1e731e57c1c9647a25760
IkHyeon <andantej281@gmail.com>
2023-07-25 11:43:25 +0900

    02_07_02_페이지에서 페이지 이동 2 1. TextButton Widget 사용해 Routing 2. Navigator.push() / .pop() / .popUntil()

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,38 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:part2/screen/new_page.dart';
 
 const assetImagePath = 'assets/images';
 const myFaceImage = '$assetImagePath/myFace.jpg';
 
 void main() {
-  // 가장 기본이 되는 앱 : MaterialApp (widget tree에 최상위에 위치
-
   runApp(const MaterialApp(
     home: HomeWidget(),
   ));
 }
 
-class HomeWidget extends StatefulWidget {
+class HomeWidget extends StatelessWidget {
   const HomeWidget({super.key});
-
-  @override
-  State<HomeWidget> createState() => _HomeWidgetState();
-}
-
-class _HomeWidgetState extends State<HomeWidget> {
-  late int index;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    index = 0;
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Flutter에서 화면 이동하기 1'),
+        title: Text('Flutter에서 화면 이동하기 2'),
         centerTitle: true,
       ),
-      body: homeBody(),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'User'),
-        ],
-        currentIndex: index,
-        onTap: (newIndex) => setState(() => index = newIndex),
+      body: Center(
+        child: TextButton(
+          child: Text('Go to Page'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const NewPage(),
+              ),
+            );
+          },
+        ),
       ),
     );
-  }
-
-  Widget homeBody() {
-    switch (index) {
-      case 1:
-        return const Center(
-          child: Icon(Icons.search, size: 100),
-        );
-      case 2:
-        return const Center(
-          child: Icon(Icons.person, size: 100),
-        );
-      case 0:
-      default:
-        return const Center(
-          child: Icon(Icons.home, size: 100),
-        );
-    }
   }
 }
---
+++ lib/screen/new_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NewPage extends StatelessWidget {
+  const NewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome New Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              child: const Text('Go to Back'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            TextButton(
+              child: const Text('Go to New Page2'),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const NewPage2()) );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NewPage2 extends StatelessWidget {
+  const NewPage2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome to New Page 2'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Go to Back'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.popUntil(context, (route) => route.isFirst),
+              child: const Text('Go to Home'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
